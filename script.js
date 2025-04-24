db2fb59686fd6d085095fbefb3bae6462dadd25c
SuperTec100 <danielm.s99@hotmail.com>
2025-04-24 20:33:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-VBHoQW0b5y0lmxRkIAj-ciAbuwF3YW8",
+  authDomain: "gef-app1.firebaseapp.com",
+  projectId: "gef-app1",
+  storageBucket: "gef-app1.firebasestorage.app",
+  messagingSenderId: "625530882269",
+  appId: "1:625530882269:web:c47d79aa16508cb855b334"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const users = JSON.parse(localStorage.getItem("users")) || {
   "admin": {
     password: "admin123",
