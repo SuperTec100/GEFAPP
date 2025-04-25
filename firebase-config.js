de09b1a3e1e4bf898bda1b79961d5da2696dedfb
SuperTec100 <danielm.s99@hotmail.com>
2025-04-25 09:50:27 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { 
+  getAuth, 
+  setPersistence, 
+  browserSessionPersistence 
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-VBHoQW0b5y0lmxRkIAj-ciAbuwF3YW8",
+  authDomain: "gef-app1.firebaseapp.com",
+  projectId: "gef-app1",
+  storageBucket: "gef-app1.appspot.com",
+  messagingSenderId: "625530882269",
+  appId: "1:625530882269:web:c47d79aa16508cb855b334"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+setPersistence(auth, browserSessionPersistence)
+  .then(() => console.log("Persistência de sessão configurada"))
+  .catch((error) => console.error("Erro na persistência:", error));
+
+export { auth };
