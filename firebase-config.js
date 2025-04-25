cd49dbfc2c1609ed21972da16adea09f566236ed
SuperTec100 <danielm.s99@hotmail.com>
2025-04-25 17:08:21 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,27 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { 
   getAuth, 
   setPersistence, 
-  browserSessionPersistence 
+  browserSessionPersistence,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { 
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  collection,
+  getDocs,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC-VBHoQW0b5y0lmxRkIAj-ciAbuwF3YW8",
   authDomain: "gef-app1.firebaseapp.com",
@@ -14,11 +31,32 @@ const firebaseConfig = {
   appId: "1:625530882269:web:c47d79aa16508cb855b334"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
+// Configura persistência de sessão
 setPersistence(auth, browserSessionPersistence)
-  .then(() => console.log("Persistência de sessão configurada"))
-  .catch((error) => console.error("Erro na persistência:", error));
+  .then(() => console.log("Persistência de sessão configurada com sucesso"))
+  .catch((error) => console.error("Erro ao configurar persistência:", error));
 
-export { auth };
+// Exporta todas as funcionalidades necessárias
+export { 
+  app,
+  auth,
+  db,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  // Exportações do Firestore
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  collection,
+  getDocs,
+  deleteDoc
+};
