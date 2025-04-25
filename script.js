86043e7ca17ea8f3feb8f1b3050493627c619103
SuperTec100 <danielm.s99@hotmail.com>
2025-04-25 09:51:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { auth } from "./firebase-config.js";
 import { 
-  getAuth, 
   signInWithEmailAndPassword, 
   createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  setPersistence,
-  browserSessionPersistence
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC-VBHoQW0b5y0lmxRkIAj-ciAbuwF3YW8",
-  authDomain: "gef-app1.firebaseapp.com",
-  projectId: "gef-app1",
-  storageBucket: "gef-app1.appspot.com",
-  messagingSenderId: "625530882269",
-  appId: "1:625530882269:web:c47d79aa16508cb855b334"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-setPersistence(auth, browserSessionPersistence)
-  .then(() => console.log("Persistência de sessão configurada"))
-  .catch((error) => console.error("Erro na persistência:", error));
 
 onAuthStateChanged(auth, (user) => {
   const currentPage = window.location.pathname.split('/').pop();
