b4dcac1de30083ddfc880e2298636988e5605f32
SuperTec100 <danielm.s99@hotmail.com>
2025-05-02 00:16:54 -0300

    Add files via upload

--- exibicao-condicional.js
+++ exibicao-condicional.js
@@ -1,3 +1,5 @@
+// exibicao-condicional.js
+
 export function mostrarOpcoesContencao() {
   const sim = document.querySelector('input[name="contencao"][value="Sim"]');
   const container = document.getElementById("opcoesContencao");
@@ -24,60 +26,20 @@ export function mostrarDrogas() {
 
 export function atualizarOpcoesRespiratorias() {
   const tipo = document.getElementById("sistemaRespiratorio")?.value;
+  const viaAerea = document.getElementById("viaAereaOptions");
+  const params = document.getElementById("ventilationParams");
 
-  const viaAereaOptions = document.getElementById("viaAereaOptions");
-  const opcoesRespiratorias = document.getElementById("opcoesRespiratorias");
-  const ventilationParams = document.getElementById("ventilationParams");
+  if (!tipo || !viaAerea || !params) return;
 
-  const opcoesEspontanea = `
-    <div class="sub-section">
-      <label>Via Aérea:</label>
-      <div class="radio-group">
-        <label><input name="viaAereaEsp" type="radio" value="Fisiológica"> Fisiológica</label>
-        <label><input name="viaAereaEsp" type="radio" value="TQT"> TQT</label>
-      </div>
-    </div>`;
-
-  const opcoesVNI = `
-    <div class="sub-section">
-      <label>Modos VNI:</label>
-      <div class="radio-group">
-        <label><input name="modoVNI" type="radio" value="BIPAP"> BIPAP</label>
-        <label><input name="modoVNI" type="radio" value="CPAP"> CPAP</label>
-      </div>
-    </div>`;
-
-  const opcoesVM = `
-    <div class="sub-section">
-      <label>Modos VM:</label>
-      <div class="radio-group">
-        <label><input name="modoVM" type="radio" value="VCV"> VCV</label>
-        <label><input name="modoVM" type="radio" value="PCV"> PCV</label>
-        <label><input name="modoVM" type="radio" value="PSV"> PSV</label>
-        <label><input name="modoVM" type="radio" value="SIMV"> SIMV</label>
-      </div>
-    </div>`;
-
-  viaAereaOptions.style.display = "none";
-  opcoesRespiratorias.innerHTML = "";
-  ventilationParams.style.display = "none";
-
-  switch (tipo) {
-    case "espontanea":
-      opcoesRespiratorias.innerHTML = opcoesEspontanea;
-      viaAereaOptions.style.display = "none";
-      ventilationParams.style.display = "none";
-      break;
-    case "vni":
-      opcoesRespiratorias.innerHTML = opcoesVNI;
-      viaAereaOptions.style.display = "none";
-      ventilationParams.style.display = "block";
-      break;
-    case "VM":
-      opcoesRespiratorias.innerHTML = opcoesVM;
-      viaAereaOptions.style.display = "block";
-      ventilationParams.style.display = "block";
-      break;
+  if (tipo === "VM") {
+    viaAerea.style.display = "block";
+    params.style.display = "block";
+  } else if (tipo === "vni") {
+    viaAerea.style.display = "none";
+    params.style.display = "block";
+  } else {
+    viaAerea.style.display = "none";
+    params.style.display = "none";
   }
 }
 
